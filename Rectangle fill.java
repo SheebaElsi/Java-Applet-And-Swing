3da5ba8433a83887d53944bc695d6552714c8499
SheebaElsi <40590295+SheebaElsi@users.noreply.github.com>
2018-08-28 11:11:46 +0530

    Add files via upload

---
+++ Rectangle fill.java
@@ -0,0 +1,77 @@
+package applet;
+
+
+
+/*
+	Set Graphics Color Example 
+	This java example shows how to set graphics color using Java AWT Color class.
+*/
+
+ 
+
+import java.applet.Applet;
+
+import java.awt.Color;
+
+import java.awt.Graphics;
+
+ 
+
+ 
+
+/*
+<applet code="SetGraphicsColorExample" width=200 height=100>
+</applet>
+*/
+
+ 
+
+ 
+
+public class rectangle extends Applet{
+
+ 
+
+	public void paint(Graphics g) {
+
+		/*
+		 * Graphic objects like lines and rectangles uses current
+		 * foreground color.
+		 * 
+		 * To change the current graphic color use
+		 * void setColor(Color c) method of Graphics Class.
+		 */
+
+		
+
+		//this will create light blue color
+
+		Color customColor = new Color(30,30,255);
+
+		
+
+		g.setColor(customColor);
+
+		g.drawLine(10,10,30,30);
+
+		
+
+		g.setColor(Color.green);
+
+		g.fillRect(40,40,40,40);
+
+		
+
+		g.setColor(Color.pink);
+
+		g.fillRect(80,80,40,40);
+
+		
+
+		g.draw3DRect(81,81,40,40,true);
+
+		
+
+	}
+
+}
